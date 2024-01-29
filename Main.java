ab2a19e9b1a6a7929531a42298b2eb7d15faca02
Olulode Olatunbosun <horlathunbhosun@gmail.com>
2024-01-29 23:20:21 +0100

    first change

---
+++ Main.java
@@ -0,0 +1,61 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+//        printItems(10);
+        printItems2(10);
+    }
+
+
+    //this is 0(n) time complexity
+    public static void printItems(int n){
+        for(int i = 0; i < n; i++){
+            System.out.println(i);
+        }
+        //this is 0(2n) time complexity
+        //first rule of big O notation is to drop the constants, and it becomes 0(n)
+        //second rule of big O notation is to drop the less significant terms which is n in this case
+        for (int j = 0; j < n; j++){
+            System.out.println(j);
+        }
+    }
+
+    //this is 0(n^2) time complexity
+    //this run n * n * n times
+    //we simplify it to 0(n^2)
+    public static void printItems2(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                for (int k = 0; k < n; k++){
+                    System.out.println(i + " " + j + " " + k);
+                }
+//                System.out.println(i + " " + j);
+            }
+        }
+    }
+
+    // drop  non-dominant terms
+    // 0(n^2 + n) becomes 0(n^2)
+    // n^2 is the dominant term
+    //n is the non-dominant term
+    // 0(n^2 + n) becomes 0(n^2)
+    public static void printItems3(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.println(i + " " + j);
+            }
+        }
+
+        for (int k = 0; k < n; k++){
+            System.out.println(k);
+        }
+    }
+
+    //0(1) constant time complexity
+    public static void addItem(int n){
+        System.out.println(n + n + n);
+    }
+
+    //
+
+}
