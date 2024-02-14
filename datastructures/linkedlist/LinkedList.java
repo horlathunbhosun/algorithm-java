d0726575b1015e91cd43fcf67921973182330eec
Olulode Olatunbosun <horlathunbhosun@gmail.com>
2024-02-14 17:57:53 +0100

    feat: changes on datastructure

--- datastructures/linkedlist/LinkedList.java
+++ datastructures/linkedlist/LinkedList.java
@@ -25,4 +25,28 @@ public class LinkedList {
       tail = newNode;
       length = 1;
     }
+
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " --> ");
+            temp = temp.next;
+        }
+
+    }
+
+    public void getHead() {
+        System.out.println("Head: "+head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: "+tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: "+length);
+    }
+
+
 }
