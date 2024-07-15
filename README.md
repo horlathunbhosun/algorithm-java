3bd5b5175ab9a2b1d6f49cbad4edd06c863b2c9e
Olulode Olatunbosun <horlathunbhosun@gmail.com>
2024-07-15 18:06:16 +0200

    feat: added set and get method to linkedlist

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 
+
+https://zerotomastery.io/cheatsheets/data-structures-and-algorithms-cheat-sheet/
+
+https://www.youtube.com/playlist?list=PLU_sdQYzUj2keVENTP0a5rdykRSgg9Wp-
+
+https://github.com/jwasham/coding-interview-university
--- datastructures/linkedlist/Main.java
+++ datastructures/linkedlist/Main.java
@@ -7,15 +7,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        LinkedList myLinkedList = new LinkedList(2);
+        LinkedList myLinkedList = new LinkedList(11);
         myLinkedList.append(3);
-        myLinkedList.getHead();
-        myLinkedList.getTail();
-        myLinkedList.getLength();
+        myLinkedList.append(23);
+        myLinkedList.append(7);
+//        myLinkedList.getHead();
+//        myLinkedList.getTail();
+//        myLinkedList.getLength();
+        myLinkedList.set(1,4);
 
-        myLinkedList.prepend(1);
+        //System.out.println(myLinkedList.get(2).value);
+        //myLinkedList.prepend(1);
 
-        myLinkedList.printList();
+      myLinkedList.printList();
 
 //        System.out.println( myLinkedList.removeLast().value);
 //        System.out.println( myLinkedList.removeLast().value);
