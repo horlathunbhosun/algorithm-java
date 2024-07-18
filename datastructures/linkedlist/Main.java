83e10d77827472e21451eaae02b9e0e4d176dffd
Olulode Olatunbosun <horlathunbhosun@gmail.com>
2024-07-18 14:43:35 +0200

    feat: fix change

--- datastructures/linkedlist/Main.java
+++ datastructures/linkedlist/Main.java
@@ -11,20 +11,13 @@ public class Main {
         myLinkedList.append(3);
         myLinkedList.append(23);
         myLinkedList.append(7);
-//        myLinkedList.getHead();
-//        myLinkedList.getTail();
-//        myLinkedList.getLength();
+
         myLinkedList.set(1,4);
-        //System.out.println(myLinkedList.get(2).value);
-        //myLinkedList.prepend(1);
+
 
       myLinkedList.printList();
 
-//        System.out.println( myLinkedList.removeLast().value);
-//        System.out.println( myLinkedList.removeLast().value);
-//        System.out.println( myLinkedList.removeLast().value);
-//        System.out.println( myLinkedList.removeLast());
-//        myLinkedList.removeLast();
+
     }
 
 
